2015b1a8cfcfedf59409ba680d303ca0cb1f5dfc
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-10-27 15:26:30 +0300

    create app

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null,
+  },
+  reducers: {},
+});
+
+export const selectUser = (state) => state.user.user;
+
+export default userSlice.reducer;
