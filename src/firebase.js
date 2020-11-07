172cfd2595e8bf69ccc6157a43126dca2b84d7e6
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-07 22:56:28 +0300

    begin conversion from mongodb-node to firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyDbgqkxPFMe4YjIAglaJGKP8leo_ygVgds',
+    authDomain: 'workout-creator-29a33.firebaseapp.com',
+    databaseURL: 'https://workout-creator-29a33.firebaseio.com',
+    projectId: 'workout-creator-29a33',
+    storageBucket: 'workout-creator-29a33.appspot.com',
+    messagingSenderId: '509611031953',
+    appId: '1:509611031953:web:92403587aa442f4ee46e30',
+    measurementId: 'G-MBTVXEK89D',
+  };
