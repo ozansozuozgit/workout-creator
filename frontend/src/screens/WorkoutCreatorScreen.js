644381db769c050a4619b3356d48936dea43f36d
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-07 11:15:04 +0300

    test call mock db

--- frontend/src/screens/WorkoutCreatorScreen.js
+++ frontend/src/screens/WorkoutCreatorScreen.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './WorkoutCreatorScreen.module.css';
+import axios from 'axios';
 
 const WorkoutCreator = () => {
   const [exerciseList, setExerciseList] = useState([]);
+
+  useEffect(() => {
+    const fetchExercises = async () => {
+      const { data } = await axios.get('/api/exercises');
+      setExerciseList(data);
+    };
+    fetchExercises();
+  }, []);
 
   return (
     <div>
