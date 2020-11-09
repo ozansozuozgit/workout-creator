30ed6fb37b4f71c105c1eb5f1e4d61cf64b2acfe
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-09 21:45:35 +0300

    add loader

---
+++ src/components/layout/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Loader.module.css';
+
+function Loader() {
+  return (
+    <div className={styles.lds_facebook}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Loader;
