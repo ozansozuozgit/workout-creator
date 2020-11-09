87baafdf47123634d6e3f502946551934047c3da
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-09 17:29:24 +0300

    update signup code

--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -1,35 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import styles from './Signup.module.css';
 import isEmail from 'validator/lib/isEmail';
-import { useDispatch } from 'react-redux';
-import { auth } from '../../firebase';
-import { setUser } from '../../features/userSlice';
 import { Link, useHistory } from 'react-router-dom';
+import { selectUser } from '../../features/userSlice';
+import { useSelector } from 'react-redux';
+import { auth } from '../../firebase';
 
 function Signup() {
   const { register, handleSubmit, errors } = useForm();
   const [loading, setLoading] = useState(false);
-  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
   const history = useHistory();
 
-  async function onSubmit(data) {
-    try {
-      setLoading(true);
-      await auth.createUserWithEmailAndPassword(data.email, data.password);
-      console.log('Signup successful.');
-      dispatch(
-        setUser({
-          username: data.username,
-          uid: auth.currentUser.uid,
-        })
-      );
-      history.push('/');
-    } catch (error) {
-      console.log(error.code);
-      console.log(error.message);
-    }
-    setLoading(false);
+  useEffect(() => {
+    if (user) history.push('/');
+  }, [user, history]);
+  
+  function onSubmit(data) {
+    auth
+      .createUserWithEmailAndPassword(data.email, data.password)
+      .catch((error) => alert(error.message));
+    console.log(user);
   }
 
   return (
@@ -37,16 +29,6 @@ function Signup() {
       <div className={styles.form_container}>
         <h1>Signup</h1>
         <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-          <label>Username</label>
-          <input
-            name="username"
-            ref={register({
-              required: true,
-              minLength: 3,
-              maxLength: 20,
-              pattern: /^[A-Za-z]+$/i,
-            })}
-          />
           {errors.username && <p>Please enter a username</p>}
           <label>Email</label>
           <input
