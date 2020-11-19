e249700da9364bbc0cd008b4a2a1ab534807b461
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-19 11:24:41 +0300

    create add exercise modal structure

--- src/components/AddExerciseModal.js
+++ src/components/AddExerciseModal.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './AddExerciseModal.module.css';
 import { setExercise, setChosenExercises } from '../features/exerciseSlice';
 import { useDispatch } from 'react-redux';
 
 function AddExerciseModal({ exercise }) {
   const dispatch = useDispatch();
+  const [sets, setSets] = useState(1);
+  const [reps, setReps] = useState(1);
 
   function handleClick() {
-    dispatch(setChosenExercises(exercise));
+    let newExercise = { ...exercise };
+    newExercise.sets = sets;
+    newExercise.reps = reps;
+    dispatch(setChosenExercises(newExercise));
     dispatch(setExercise(null));
+  }
+
+  function handleInput(e) {
+    if (e.target.name === 'sets') {
+      setSets(e.target.value);
+    } else setReps(e.target.value);
   }
 
   return (
     <div className={styles.container}>
-      <h1>Modal</h1>
-      <button onClick={handleClick}>Close</button>
+      <div className={styles.info_container}>
+        <div className={styles.exerciseInfo}>
+          <h3>{exercise.name}</h3>
+          <img src={exercise.imageURL} alt="" />
+        </div>
+        <div className={styles.sets_reps_container}>
+          <label htmlFor="sets">Sets</label>
+          <input
+            type="text"
+            name="sets"
+            id="sets"
+            onChange={handleInput}
+            placeholder="SETS"
+          />
+          <label htmlFor="reps">Reps</label>
+          <input
+            type="text"
+            name="reps"
+            id="reps"
+            onChange={handleInput}
+            placeholder="REPS"
+          />
+        </div>
+        <button onClick={handleClick}>Close</button>
+      </div>
     </div>
   );
 }
