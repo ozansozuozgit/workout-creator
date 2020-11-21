f29584656ababa46fc1bf189733c5f51adac7b5e
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-21 13:43:26 +0300

    create Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { auth } from '../firebase';
+import { Link } from 'react-router-dom';
+import { selectUser } from '../features/userSlice';
+import { useSelector } from 'react-redux';
 
 function Navbar() {
+  const user = useSelector(selectUser);
+
   return (
     <div>
-      <h1>Navbar</h1>
+      <h2>Hello: {user.email}</h2>
+      <Link to="/workout">create workout</Link>{' '}
+      <button onClick={() => auth.signOut()}>Sign out</button>
     </div>
   );
 }
