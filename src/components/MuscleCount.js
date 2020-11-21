5ead30dd0949de2abda35b068fb958aa6a799572
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-21 21:44:36 +0300

    add animations and percentage progress bar

--- src/components/MuscleCount.js
+++ src/components/MuscleCount.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ProgressBar from './ProgressBar';
 import styles from './MuscleCount.module.css';
 
-function MuscleCount({ muscle }) {
+function MuscleCount({ muscle, sum }) {
   return (
     <div className={styles.container}>
       <p>{muscle[0]}</p>
       <ProgressBar
         bgcolor={`#${Math.floor(Math.random() * 16777215).toString(16)}`}
-        completed={muscle[1] * 5}
+        completed={Math.round((muscle[1] / sum) * 100)}
       />
     </div>
   );
--- src/components/MuscleCountList.js
+++ src/components/MuscleCountList.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import styles from './MuscleCountList.module.css';
 import MuscleCount from './MuscleCount';
+import { motion } from 'framer-motion';
 
-function MuscleCountList({ workedMuscles }) {
+function MuscleCountList({ workedMuscles, sum }) {
+  const containerVariants = {
+    hidden: {
+      scale: 0,
+    },
+    visible: {
+      scale: 1,
+      transition: {
+        duration: 3,
+        type: 'spring',
+        bounce: 0.5,
+      },
+    },
+    exit: {
+      scale: 0,
+    },
+  };
+
   return (
-    <div className={styles.container}>
-      {workedMuscles.map((muscle, index) => (
-        <MuscleCount key={index} muscle={muscle} />
-      ))}
-    </div>
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <div className={styles.container}>
+        {workedMuscles.map((muscle, index) => (
+          <MuscleCount key={index} muscle={muscle} sum={sum} />
+        ))}
+      </div>
+    </motion.div>
   );
 }
 
