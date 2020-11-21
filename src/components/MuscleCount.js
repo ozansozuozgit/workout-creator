667a48532b10153b74ce98864a0357059f3df3a4
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-21 19:19:16 +0300

    add progressbar to musclecount

--- src/components/MuscleCount.js
+++ src/components/MuscleCount.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import ProgressBar from './ProgressBar';
+import styles from './MuscleCount.module.css';
 
 function MuscleCount({ muscle }) {
   return (
-    <div>
-      <p>
-        {muscle[0]}:{muscle[1]}
-      </p>
+    <div className={styles.container}>
+      <p>{muscle[0]}</p>
+      <ProgressBar
+        bgcolor={`#${Math.floor(Math.random() * 16777215).toString(16)}`}
+        completed={muscle[1] * 5}
+      />
     </div>
   );
 }
