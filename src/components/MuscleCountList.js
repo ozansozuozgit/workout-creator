be8bd545cb7b612aba8f91898b60e42de11ec8e6
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-21 13:44:22 +0300

    create MuscleCountComponent

---
+++ src/components/MuscleCountList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './MuscleCountList.module.css';
+import MuscleCount from './MuscleCount';
+
+function MuscleCountList({ workedMuscles }) {
+  return (
+    <div className={styles.container}>
+      {workedMuscles.map((muscle, index) => (
+        <MuscleCount key={index} muscle={muscle} />
+      ))}
+    </div>
+  );
+}
+
+export default MuscleCountList;
