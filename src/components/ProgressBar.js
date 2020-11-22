78766f689ae5f2cfffbdd38e204f27925d455be8
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-22 14:07:04 +0300

    add calculation logic in progress bar with timeout

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { selectTotalMuscleCount } from '../features/workoutsSlice';
+import { useSelector } from 'react-redux';
 
-function ProgressBar({ bgcolor, completed }) {
+function ProgressBar({ bgcolor, muscleCount }) {
+  const [completedWith, setCompletedWith] = useState(0);
+  const sum = useSelector(selectTotalMuscleCount);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCompletedWith(Math.round((muscleCount / sum) * 100));
+    }, 500);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sum]);
   const containerStyles = {
     height: 20,
     width: '100%',
     backgroundColor: '#e0e0de',
     borderRadius: 50,
+    boxShadow: '0 0 5px black',
   };
 
   const fillerStyles = {
     height: '100%',
-    width: `${completed}%`,
+    width: `${completedWith}%`,
     backgroundColor: bgcolor,
     borderRadius: 'inherit',
     textAlign: 'right',
-    transition: 'width 1s ease-in-out',
+    transition: 'width 2s ease-in-out',
   };
 
   return (
